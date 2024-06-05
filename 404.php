a31d6ad5e276615bae4fa258f959e818a6246399
nadrojmit <64118633+nadrojmit@users.noreply.github.com>
2024-06-05 10:36:32 +0930

    Replacing blank slate with this is learning

--- 404.php
+++ 404.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 <article id="post-0" class="post not-found">
 <header class="header">
-<h1 class="entry-title" itemprop="name"><?php esc_html_e( 'Page not found', 'blankslate' ); ?></h1>
+<h1 class="entry-title" itemprop="name"><?php esc_html_e( 'Page not found', 'thisislearning' ); ?></h1>
 </header>
 <div class="entry-content" itemprop="mainContentOfPage">
-<p><?php esc_html_e( 'Sorry, we can\'t find that page for you.', 'blankslate' ); ?></p>
+<p><?php esc_html_e( 'Sorry, we can\'t find that page for you.', 'thisislearning' ); ?></p>
 </div>
 </article>
 <?php get_footer(); ?>
